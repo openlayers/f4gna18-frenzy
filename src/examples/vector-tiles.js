b2a16f1c091f8369ac35d5901d3b3ad598cead83
ahocevar <andreas.hocevar@gmail.com>
2018-05-15 10:07:09 -0500

    Drag/drop in vector tiles exampe

--- src/examples/vector-tiles.js
+++ src/examples/vector-tiles.js
@@ -1,18 +1,31 @@
 import 'ol/ol.css';
-import bright from './bright.json';
+import bright from './data/bright.json';
 import {apply} from 'ol-mapbox-style';
 import VectorTileLayer from 'ol/layer/VectorTile';
+import dragdrop from 'drag-drop/buffer';
 
 const map = apply('map-container', bright);
 
-let declutter = true;
-document.getElementById('declutter').addEventListener('click', function() {
+function declutter(declutter) {
   const layer = map.getLayers().item(0);
   map.removeLayer(layer);
-  declutter = !declutter;
   map.addLayer(new VectorTileLayer({
     source: layer.getSource(),
     style: layer.getStyle(),
     declutter: declutter
   }));
+}
+
+document.getElementById('clutter').addEventListener('click', function() {
+  declutter(false);
+});
+document.getElementById('declutter').addEventListener('click', function() {
+  declutter(true);
+});
+
+
+dragdrop('#map-container', function(files) {
+  map.getLayers().clear();
+  const style = files[0].toString().replace('{key}', 'ER67WIiPdCQvhgsUjoWK');
+  apply(map, JSON.parse(style));
 });
