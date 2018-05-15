b01875b38f81f97eb6a8222fa7a36b340dffbe21
ahocevar <andreas.hocevar@gmail.com>
2018-05-15 15:52:52 -0500

    Add click interactivity to vector tiles exaple

--- src/examples/vector-tiles.js
+++ src/examples/vector-tiles.js
@@ -2,6 +2,7 @@ import 'ol/ol.css';
 import bright from './data/bright.json';
 import {apply} from 'ol-mapbox-style';
 import VectorTileLayer from 'ol/layer/VectorTile';
+import Overlay from 'ol/Overlay';
 import dragdrop from 'drag-drop/buffer';
 
 const map = apply('map-container', bright);
@@ -23,9 +24,28 @@ document.getElementById('declutter').addEventListener('click', function() {
   declutter(true);
 });
 
-
 dragdrop('#map-container', function(files) {
   map.getLayers().clear();
   const style = files[0].toString().replace('{key}', 'ER67WIiPdCQvhgsUjoWK');
   apply(map, JSON.parse(style));
+});
+
+const overlayDiv = document.getElementById('overlay');
+const overlay = new Overlay({
+  element: overlayDiv,
+  positioning: 'bottom-center'
+});
+map.addOverlay(overlay);
+overlayDiv.addEventListener('click', function() {
+  overlay.setPosition();
+});
+
+map.on('click', function(e) {
+  const features = map.getFeaturesAtPixel(e.pixel);
+  if (features && features[0].get('class')) {
+    overlay.setPosition(e.coordinate);
+    overlayDiv.innerHTML = features[0].get('layer') + ' ' + features[0].get('class');
+  } else {
+    overlay.setPosition();
+  }
 });
