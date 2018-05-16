49a97c81f90cedd0ea2cc3711aeceb8c611dc578
Tim Schaub <tim.schaub@gmail.com>
2018-05-16 08:31:22 -0500

    Feature style update

--- src/feature-style.js
+++ src/feature-style.js
@@ -8,11 +8,20 @@ import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
 import sync from 'ol-hashed';
 import View from 'ol/View';
+import Overlay from 'ol/Overlay';
 
 const source = new VectorSource({
   format: new GeoJSON(),
   url: dataURL
 });
+
+const yearLookup = {
+  108: '2003–2004',
+  109: '2005–2006',
+  110: '2007–2008',
+  111: '2009–2010',
+  112: '2011–2012'
+};
 
 const minSession = 108;
 const maxSession = 112;
@@ -21,7 +30,7 @@ let selectedSession;
 const output = document.getElementById('output');
 for (let session = maxSession; session >= minSession; --session) {
   const anchor = document.createElement('a');
-  anchor.innerHTML = session;
+  anchor.innerHTML = yearLookup[session];
   anchor.dataset.session = session;
   anchor.addEventListener('mouseover', event => {
     selectedSession = event.target.dataset.session;
@@ -65,7 +74,7 @@ const map = new Map({
         const t = r + d;
         return new Style({
           fill: new Fill({
-            color: [r * 255 / t, 0, d * 255 / t, 0.5]
+            color: [r * 255 / t, 0, d * 255 / t, 0.6]
           }),
           stroke: new Stroke({
             color: [255, 255, 255, 0.6]
@@ -75,7 +84,7 @@ const map = new Map({
     }),
     new TileLayer({
       source: new XYZSource({
-        url: 'https://api.mapbox.com/styles/v1/tschaub/cjh75hlvk00902rquajmff74y/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidHNjaGF1YiIsImEiOiJjaW5zYW5lNHkxMTNmdWttM3JyOHZtMmNtIn0.CDIBD8H-G2Gf-cPkIuWtRg'
+        url: 'https://api.mapbox.com/styles/v1/tschaub/cjh953yhw1b2l2rqwpbglpnxq/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidHNjaGF1YiIsImEiOiJjaW5zYW5lNHkxMTNmdWttM3JyOHZtMmNtIn0.CDIBD8H-G2Gf-cPkIuWtRg'
       })
     })
   ],
@@ -83,6 +92,54 @@ const map = new Map({
     center: fromLonLat([-79.093, 35.224]),
     zoom: 7
   })
+});
+
+const container = document.createElement('div');
+container.className = 'overlay';
+const overlay = new Overlay({
+  element: container,
+  positioning: 'bottom-center',
+  autoPan: true
+});
+map.addOverlay(overlay);
+container.addEventListener('click', function() {
+  overlay.setPosition();
+});
+
+map.on('pointermove', function(event) {
+  if (map.hasFeatureAtPixel(event.pixel)) {
+    document.body.style.cursor = 'pointer';
+  } else {
+    document.body.style.cursor = '';
+  }
+});
+
+map.on('click', function(event) {
+  const features = map.getFeaturesAtPixel(event.pixel);
+  if (features) {
+    overlay.setPosition(event.coordinate);
+    const member = features[0].get('member');
+
+    let markup = '<table>';
+    Object.keys(member).sort().reverse().forEach(function(session) {
+      const lookup = member[session];
+      for (const key in lookup) {
+        const person = lookup[key];
+        markup += `
+          <tr>
+            <td><div class="dot ${person.party}">&nbsp;</div></td>
+            <td>${person.name}</td>
+            <td>${yearLookup[session]}</td>
+          </tr>
+        `;
+      }
+    });
+    markup += '</table>';
+
+    container.innerHTML = markup;
+  } else {
+    overlay.setPosition();
+  }
 });
 
 sync(map);
